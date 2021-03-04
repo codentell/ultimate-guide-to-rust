d5bf05ea357101c607598fe770c2a197b980ed46
drewhoang <drewhoang@Stevens-Air-2.localdomain>
2021-03-03 19:43:48 -0800

    hello rust

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+/Users/drewhoang/Desktop/ultimate-guide-to-rust/hello/target/debug/deps/hello: src/main.rs
+
+/Users/drewhoang/Desktop/ultimate-guide-to-rust/hello/target/debug/deps/hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/drewhoang/Desktop/ultimate-guide-to-rust/hello/target/debug/hello: /Users/drewhoang/Desktop/ultimate-guide-to-rust/hello/src/main.rs
